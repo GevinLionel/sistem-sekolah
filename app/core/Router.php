5eb54b0afcc39b864d80b87cce086c91f20230ce
Gevin Lionel <alamlau80@gmail.com>
2026-02-24 14:18:30 +0700

    iyah

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,31 +1,83 @@
 <?php
 namespace App\Core;
 
+use App\controllers\StudedentController;
+
+
 
 class Router
 {
+    private array $routes = [];
 
-    public function run(): void
+    public function add(string $method, string $path, string $controller, string $function)
+    {
+        $this->routes[] = [
+            'method'     => $method,
+            'path'       => $path,
+            'controller' => $controller,
+            'function'   => $function
+        ];
+    }
+
+    private function buildPattern(string $path)
+    {
+        $pattern = str_replace(
+            '{id}',
+            '([0-9]+)',
+            $path,
+        );
+        return '#^' . $pattern . '$#';
+    }
+
+    public function run()
     {
         $method = $_SERVER['REQUEST_METHOD'];
         $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-        if($method == 'GET' && $uri == '/students'){
-            echo '<h1>Daftar siswa</h1>';
-            echo '<p>Menampilkan daftar siswa</p>';
-            return;
+        foreach ($this->routes as $route) {
+            $pattern = $this->buildPattern($route['path']);
+
+            if (preg_match($pattern, $uri, $matches)) {
+                array_shift($matches);
+
+                require_once './app/controllers/' . $route['controller'] . '.php';
+                $function = $route['function'];
+
+                $controllerClass = 'App\\Controllers\\' . $route['controller'];
+                $controller = new $controllerClass();
+
+                call_user_func_array([$controller, $function], $matches);
+
+                return;
+            }
         }
-
-        if($method == 'GET' && $uri == '/students/create'){
-            echo '<h1>Tambah Siswa</h1>';
-            echo '<p>Menampilkan form tambah siswa</p>';
-            return;
-        }
-
-        http_response_code(404);
-        echo '<h1>404 - tak ade pagenya putar balek kau</h1>';
-
     }
-
 }
+
+class Router
+{
+    public function run()
+    {
+        $method = $_SERVER['REQUEST_METHOD'];
+        $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        if ($method == 'GET' && $uri == '/students') {
+        require_once '../app/controllers/StudentController.php';
+        $controller = new StudentController();
+        $controller->index();
+return;
+}
+
+if ($method == 'GET' && $uri == '/students/create') {
+require_once '../app/controllers/StudentController.php';
+$controller = new StudentController();
+$controller->create();
+return;
+}
+http_response_code(404);
+echo "Not Found Page";
+}
+}
+
+
+
 ?>
