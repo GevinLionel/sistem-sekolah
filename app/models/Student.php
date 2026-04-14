f6e3684d93ef56878428ef245c41b6ff5d809993
Gevin Lionel <alamlau80@gmail.com>
2026-04-14 14:31:10 +0700

    wip: kiw

---
+++ app/models/Student.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+require_once '../app/core/database.php';
+
+use App\Core\Database;
+
+class Student extends Database
+{
+    protected $table = 'students'; 
+
+    public function getstudent()
+    {
+        $students = [];
+        
+        $query = "SELECT * FROM " . $this->table;
+        $snmt = $this->connection->prepare($query);
+        $snmt->execute();
+        $result = $snmt->get_result();
+        while ($student = $result->fetch_assoc()) {
+            $students[] = $student;
+        }
+        return $students;
+    }   
+
+require_once '../app/core/Controller.php';
+require_once '../app/core/Student.php';
+
+use App\Core\Controller;
+use App\Models\Student;
+
+
+?>
