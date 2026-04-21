9be4fb1a869061e36d7eb7c0439a5c5e70d292cf
Gevin Lionel <alamlau80@gmail.com>
2026-04-21 14:28:29 +0700

    a

--- public/index.php
+++ public/index.php
@@ -4,5 +4,14 @@ require_once '  ./app/core/Router.php';
 use App\core\Router;
 
 $router = new Router();
+
+
+//regestrasi route
+$router->add('GET', '/students', 'StudentController', 'index');
+$router->add('GET', '/students/create', 'StudentController', 'create');
+$router->add('GET', '/students/{id}', 'StudentController', 'show');
+
+$router->add('POST', '/students', 'StudentController', 'store');
+
 $router->run();
 ?>
